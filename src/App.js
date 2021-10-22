49de2764c95b059d34364cec8cba25859d1d3e3f
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-10-22 17:22:40 -0400

    sets up a +/- counter app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      Hello, its me
-    </div>
-  );
-}
+import "./App.css";
+
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  
+  };
+
+  addCount = () => {
+    console.log("add is clicked")
+    this.setState({
+      count: this.state.count + 1,
+    })
+  };
+
+  minusCount = () => {
+    console.log("minus is clicked")
+
+    this.setState({
+      count: this.state.count -1,
+    })
+  };
+
+  render() {
+    return (
+      <div className="counter-container">
+        <div>
+        Count: {this.state.count}
+        </div>
+        <button onClick={this.minusCount}>-</button>
+        <button onClick={this.addCount}>+</button>
+      </div>
+    );
+  };
+};
 
 export default App;
+
+
+
+// ===============
+  // The below is the standard of what we get when we create react app. Above is a more react-centric approach
+// ===============
+
+
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello, its me
+//     </div>
+//   );
+// }
+
+// export default App;
