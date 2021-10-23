03fc6207d0f0cf9a48b50492845f774e606b5fd8
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-10-23 15:43:27 -0400

    preps for edit functionality

--- src/components/Todo/TodoList.js
+++ src/components/Todo/TodoList.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import "./TodoList.css";
 
 export class TodoList extends Component {
-
+   state = {
+      canEdit: false,
+   }
    render() {
       // console.log(this.props.handleDeleteByID)
       const { todo, id, isDone } = this.props.item;
@@ -11,7 +13,9 @@ export class TodoList extends Component {
          <div style={{display: "flex", justifyContent: "center", listStyle: "none",}}>
             <li className={`${isDone && `li-done`}`}>{todo}</li>
             <button id="edit-button">Edit</button>
+            
             <button id="done-button" onClick={() => handleDoneByID(id, isDone)}>Done</button>
+            
             <button id="delete-button" onClick={() => handleDeleteByID(id)}>Delete</button>
          </div>
       )
